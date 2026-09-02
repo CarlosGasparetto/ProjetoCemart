b01eb83692684e7a1199b3a76657192f69e1d101
Carlos Gasparetto <ca.gasparetto@gmail.com>
2026-09-02 11:50:24 -0300

    Inserção da interface

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+// supabase-client.js
+const SUPABASE_URL = 'https://SEU-PROJETO.supabase.co';
+const SUPABASE_ANON_KEY = 'SUA-CHAVE-ANON-AQUI';
+
+// Inicializa o cliente do Supabase
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
