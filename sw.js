e5d57266dbb39fd3a5c5bf06e9ca1777022d4fb2
Carlos Gasparetto <ca.gasparetto@gmail.com>
2026-09-02 17:50:19 -0300

    atualização no arquivo sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,37 @@
-// sw.js - Permite instalação como PWA
+const CACHE_NAME = 'cemart-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './inscricao.html',
+  './consulta.html',
+  './manifest.json',
+  './supabase-client.js'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
