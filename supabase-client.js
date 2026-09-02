753dad67253fe3e0f52a5a2ab703a9174adec84e
Carlos Gasparetto <ca.gasparetto@gmail.com>
2026-09-02 12:46:38 -0300

    Atualização dos arquivo html
    Adequação ao banco de dados elaborado no supabase

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // supabase-client.js
-const SUPABASE_URL = 'https://SEU-PROJETO.supabase.co';
-const SUPABASE_ANON_KEY = 'SUA-CHAVE-ANON-AQUI';
+const SUPABASE_URL = 'https://fpvmtogbgjgobudxibsg.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_4pNC3GHjo_266FMpopQMmw_a-P0swpF';
 
 // Inicializa o cliente do Supabase
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
